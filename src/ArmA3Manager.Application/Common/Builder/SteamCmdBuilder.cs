2a6da6df04f0b046a34c2897ae212328985fbf17
8-prime <hoffmeister.stefan99@gmail.com>
2025-12-04 14:44:03 +0100

    Add config bundles. Default config creation. Config bundle management ui

---
+++ src/ArmA3Manager.Application/Common/Builder/SteamCmdBuilder.cs
@@ -0,0 +1,6 @@
+﻿namespace ArmA3Manager.Application.Common.Builder;
+
+public class SteamCmdBuilder
+{
+    
+}
---
+++ src/ArmA3Manager.Application/Common/Extensions/MissionInfoExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace ArmA3Manager.Application.Common.Extensions;
+
+public class MissionInfoExtensions
+{
+    
+}
